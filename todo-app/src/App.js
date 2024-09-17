d73911fe77e18b2f1770185a52d4eaabba8c25ac
[David Ouma] <[daviddae438@gmail.com]>
2024-09-17 05:07:15 -0700

    Added a react code

---
+++ todo-app/src/App.js
@@ -0,0 +1,36 @@
+import Raect, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState('');
+
+  // Function to handle task addition 
+  const addTask = () => {
+    if (input) {
+      setTasks([...tasks, input]);
+      setInput('');
+    }
+  };
+
+  return (
+    <div className= "App">
+      <h1>To-Do List</h1>
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button onClick={addTask}>add</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
